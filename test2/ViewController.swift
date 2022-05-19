2527055ba674f2b06094060aa3c0213c5309c7c9
muker <rawgmuker@naver.com>
2022-05-19 11:34:54 +0900

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Muker on 2022/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        hello()
+    }
+    
+    func hello() {
+        print("hello")
+    }
+
+}
+
